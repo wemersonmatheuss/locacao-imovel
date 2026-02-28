9a3311b686ea3361c53556de01b23fd603d30f66
wemersonmatheuss <wemersonmthdev@gmail.com>
2026-02-28 17:44:15 -0300

    feat: add login page components

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,13 @@
 import styles from "./styles.module.css"
-
 import { AuthForm } from "../../components/AuthForm"
+import background from "../../assets/image-background.png"
 
 export function Login() {
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      style={{ backgroundImage: `url(${background})` }}
+    >
       <AuthForm type="login" />
     </div>
   )
