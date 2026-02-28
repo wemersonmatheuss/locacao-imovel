165725e3f9a5bfd298968918bcc71573c06e03fe
wemersonmatheuss <wemersonmthdev@gmail.com>
2026-02-28 17:44:24 -0300

    feat: add register page components

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,9 +1,14 @@
 import styles from "./styles.module.css"
+import { AuthForm } from "../../components/AuthForm"
+import background from "../../assets/image-background.png"
 
 export function Register() {
   return (
-    <div className={styles.container}>
-      <h1>Cadastro</h1>
+    <div
+      className={styles.container}
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <AuthForm type="register" />
     </div>
   )
 }
